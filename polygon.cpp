533807bebfa852049be096ea00e736def4be2f22
Jason Warta <jasonwarta@gmail.com>
2016-04-13 20:19:24 -0800

    pushed polygon.cpp

---
+++ polygon.cpp
@@ -0,0 +1,21 @@
+/*
+ * polygon.cpp
+ * 4/13/2016
+ * 
+ * CS 372 Sp16
+ * Group Project 2: Postscript Generator
+ */
+
+#include "polygon.h"
+
+string Polygon::draw(int x,int y){
+	return "";
+}
+
+string Square::draw(int x,int y){
+	return "";
+}
+
+string Triangle::draw(int x,int y){
+	return "";
+}
