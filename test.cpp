3a60b2e0e56c0c705b7db7d10747e463e2705427
Arsh Chauhan <achauhan@alaska.edu>
2016-04-15 11:23:21 -0800

    Added polygon tests

--- test.cpp
+++ test.cpp
@@ -2,8 +2,40 @@
 #include "catch.hpp"
 #include <string>
 using std::string;
+using std::make_pair;
 
 #include "rectangle.h"
+#include "polygon.h"
+
+pair<double,double> testGetPoint (int k, int sides, double length)
+{
+	double x = (length/2)*(sin((((2*k)+1)*PI)/sides))/(sin(PI/sides));
+	double y = (-length/2)*(cos((((2*k)+1)*PI)/sides))/(sin(PI/sides));
+	return make_pair(x,y);
+
+}
+
+string testPolyDraw (int x, int y, int sides, int length)
+{
+	stringstream ss;
+	ss << "gsave\n";
+	ss << "newpath\n";
+	ss << x <<" " <<y << " translate\n";
+	pair<double,double> vertex = testGetPoint(0,sides,length);
+	ss << vertex.first << " "<< vertex.second << " moveto\n";
+
+	for(int i = 1; i < sides; i++){
+		vertex = testGetPoint(i,sides,length);
+		ss << vertex.first << " " << vertex.second << " lineto\n";
+	}
+
+	ss << "closepath\n";
+	ss << "stroke\n";
+	ss << "grestore\n";
+
+	return ss.str();
+
+}
 
 TEST_CASE ("Rectangle Construction","[Rectangle]")
 {
@@ -45,4 +77,20 @@ TEST_CASE ("Rectangle Construction","[Rectangle]")
 
 	postScript = rect.draw(x,y);
 	REQUIRE(postScript == expectedPS.str());
+}
+
+TEST_CASE("Polygon Draw","[Polygon]")
+{
+    //numOfSides, sidelength
+	Polygon poly1(1,1); 
+	Polygon poly2(1,1);
+	Polygon poly3(4,1); //Square
+	Polygon poly4(3,10); //Triangle 
+	Polygon poly5(10,10);
+	Polygon poly6(10,23.6);
+
+	REQUIRE(poly1.draw(72,72) == testPolyDraw(72,72,1,1));
+	REQUIRE(poly2.draw(72,144) == testPolyDraw(72,144,1,1));
+
+
 }
